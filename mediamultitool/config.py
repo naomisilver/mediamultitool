b9838358617f9cf59f9621ea6162b7e7dbdcc3c8
naomisilver <2425281@dundee.ac.uk>
2026-02-13 21:21:07 +0000

    Error correcting from a reverted merge
    Readding commits from dev that should've been merged in the last merge commit but missed because of that silly github issue of wanting to merge into main and not dev.

--- mediamultitool/config.py
+++ mediamultitool/config.py
@@ -30,6 +30,7 @@ class PlaylistConfig:
     blocklist_strings: str = ''
     allowlist_strings: str = ''
     default_output: str = ''
+    artist_aliases: dict[str, str] = field(default_factory=dict)
 
 @dataclass(slots=True)
 class CleanerConfig:
@@ -37,7 +38,7 @@ class CleanerConfig:
 
 @dataclass(slots=True)
 class Misc:
-    version: str = '0.1.2'
+    version: str = '0.1.3'
 
 @dataclass(slots=True)
 class AppConfig:
